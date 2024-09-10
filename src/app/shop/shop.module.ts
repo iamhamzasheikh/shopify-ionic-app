023aae7156fffeebc50eea4bfa49e88fc23bcea5
Hamza Sheikh <zafarhamza789@gmail.com>
2024-09-10 10:43:25 +0500

    start-third-page

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,6 +11,7 @@ import { ShopPageRoutingModule } from './shop-routing.module';
 import { ShopPage } from './shop.page';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
--- src/app/shop/shop.page.ts
+++ src/app/shop/shop.page.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.page.html',
   styleUrls: ['./shop.page.scss'],
 })
-export class ShopPage implements OnInit {
+export class ShopPage  {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
