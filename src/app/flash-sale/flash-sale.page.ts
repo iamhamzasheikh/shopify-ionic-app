4f0bbd0e3e17ed92598158f44223bb1429a2a4e1
Hamza Sheikh <zafarhamza789@gmail.com>
2024-09-14 11:31:01 +0500

    5th

--- src/app/flash-sale/flash-sale.page.ts
+++ src/app/flash-sale/flash-sale.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-flash-sale',
   templateUrl: './flash-sale.page.html',
   styleUrls: ['./flash-sale.page.scss'],
 })
-export class FlashSalePage implements OnInit {
+export class FlashSalePage implements OnInit, OnDestroy {
+  minutes: number = 0;
+  seconds: number = 0;
+  private timerSubscription: Subscription | undefined;
 
   constructor() { }
 
   ngOnInit() {
+    this.startTimer();
   }
 
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  startTimer() {
+    this.timerSubscription = interval(1000).subscribe(() => {
+      this.seconds++;
+      if (this.seconds === 60) {
+        this.seconds = 0;
+        this.minutes++;
+      }
+    });
+  }
+
+  stopTimer() {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+      this.timerSubscription = undefined;
+    }
+  }
 }
