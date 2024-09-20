22b2e2a19d76900842fb53e9d945cc3f879b3515
Hamza Sheikh <zafarhamza789@gmail.com>
2024-09-20 15:49:19 +0500

    6th

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
 })
-export class CartPage implements OnInit {
+export class CartPage {
+
+  products = [
+    { name: 'Product 1', description: 'Lorem ipsum dolor sit amet consectetur.', color: 'Pink', size: 'M', quantity: 1, image: '../../assets/cart1.png' },
+    { name: 'Product 2', description: 'Adipisicing elit.', color: 'Blue', size: 'L', quantity: 1, image: '../../assets/cart2.png' },
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  decrement(product: any) {
+    if (product.quantity > 1) {
+      product.quantity--;
+    }
   }
 
+  increment(product: any) {
+    product.quantity++;
+  }
 }
