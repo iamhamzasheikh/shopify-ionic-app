fad6ffc8edb402f398d30b150e0346de1726239e
Hamza Sheikh <zafarhamza789@gmail.com>
2024-10-30 23:00:25 +0500

    status-change-done

--- src/app/status/status.page.ts
+++ src/app/status/status.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalService } from '../services/global.service';
 
 @Component({
   selector: 'app-status',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status.page.scss'],
 })
 export class StatusPage implements OnInit {
+  selectedSegment: string = 'shipped';
+  cartProducts: any[] = []; // Array to hold all products
+  deliveredProducts: any[] = []; // New array for delivered products
 
-  constructor() { }
+  constructor(private globalService: GlobalService) {}
 
   ngOnInit() {
+    // Fetch all products from the global service
+    this.cartProducts = this.globalService.getCartProducts();
   }
 
+  // Method to handle status change
+  changeStatus(item: any) {
+    // Remove item from cartProducts and add to deliveredProducts
+    this.cartProducts = this.cartProducts.filter(cartItem => cartItem !== item);
+    this.deliveredProducts.push(item);
+  }
 }
